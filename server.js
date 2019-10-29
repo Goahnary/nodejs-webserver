3a72725158b094965f86987b99f5d57fb64166c1
goahnary <ubuntu@ip-172-31-26-6.us-west-2.compute.internal>
2019-10-29 22:29:58 +0000

    typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 function onRequest(request, response) {
-	response.writeHead(200, {'Content-Type': 'applicatio/json'});
+	response.writeHead(200, {'Content-Type': 'application/json'});
 	response.write('{"text":"Hello, world!"}');
 	response.end();
 }
