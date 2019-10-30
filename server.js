95ca0a0ee05e55974074bffa38a89d4f92dfce6d
Noah Gary <ubuntu@ip-172-31-26-6.us-west-2.compute.internal>
2019-10-30 01:04:30 +0000

    serving images but it is broken...

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
-var http = require('http');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+var dir = path.join(__dirname, 'html');
+
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
 
 function onRequest(request, response) {
-	response.writeHead(200, {'Content-Type': 'application/json'});
-	response.write('{"text":"Hello, world!"}');
+
+	var reqpath = request.url.toString().split('?')[0];
+
+	if (request.method !== 'GET') {
+        response.statusCode = 501;
+        response.setHeader('Content-Type', 'text/plain');
+        return response.end('Method not implemented');
+    }
+	
+	if (fs.existsSync('html' + request.url) || request.url === '/' || fs.existsSync('html' + request.url + '/index.html'))
+	//Does the file exist?
+	{
+		var file = path.join(dir, reqpath.replace(/\/$/, '/index.html'));
+
+		var type = mime[path.extname(file).slice(1)] || 'text/plain';
+
+	    response.writeHead(200, {"Content-Type": type});
+
+		var html = fs.readFileSync(file, 'utf8');
+		
+		response.write(html);
+
+	} else {
+		
+		response.writeHead(404, {"Content-Type": "text/html"});
+
+		//get 404 page
+		var four0four = fs.readFileSync('html/404.html', 'utf8');
+
+		response.write(four0four);
+	}
 	response.end();
 }
 
